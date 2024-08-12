569830b07207710fa5f85506744fe02a74c2d849
msnaoum <56797400+msnaoum@users.noreply.github.com>
2024-08-13 01:23:20 +0300

    refactor

--- BankingControlPanel.Application/Services/ClientService.cs
+++ BankingControlPanel.Application/Services/ClientService.cs
@@ -1,61 +1,49 @@
 using AutoMapper;
-using BankingControlPanel.Application.Mapper;
 using BankingControlPanel.Domain.Models;
-using BankingControlPanel.Infrastructure.Repositories;
 using BankingControlPanel.Interfaces.Repositories;
 using BankingControlPanel.Interfaces.Services;
 using BankingControlPanel.Shared.Dtos;
 
 namespace BankingControlPanel.Application.Services;
 
-public class ClientService : IClientService
+public class ClientService(IClientRepository clientRepository, IMapper mapper) : IClientService
 {
-    private readonly IClientRepository _clientRepository;
-    private readonly IMapper _mapper;
-
-    public ClientService(IClientRepository clientRepository, IMapper mapper)
-    {
-        _clientRepository = clientRepository;
-        var config = new MapperConfiguration(cfg => cfg.AddProfile<MappingProfile>());
-        _mapper = config.CreateMapper();
-    }
-
     public IEnumerable<ClientDto> GetClients(string filter, string sort, int page, int pageSize)
     {
-        var clients = _clientRepository.GetClients(filter, sort, page, pageSize);
-        return _mapper.Map<IEnumerable<ClientDto>>(clients);
+        var clients = clientRepository.GetClients(filter, sort, page, pageSize);
+        return mapper.Map<IEnumerable<ClientDto>>(clients);
     }
 
     public ClientDto GetClientById(int id)
     {
-        var client = _clientRepository.GetClientById(id);
-        return _mapper.Map<ClientDto>(client);
+        var client = clientRepository.GetClientById(id);
+        return mapper.Map<ClientDto>(client);
     }
 
     public Task AddClientAsync(ClientDto clientDto)
     {
-        var client = _mapper.Map<Client>(clientDto);
-        _clientRepository.AddClient(client);
+        var client = mapper.Map<Client>(clientDto);
+        clientRepository.AddClient(client);
         return Task.CompletedTask;
     }
 
     public void UpdateClient(int id, ClientDto clientDto)
     {
-        var client = _clientRepository.GetClientById(id);
+        var client = clientRepository.GetClientById(id);
         if (client != null)
         {
-            _mapper.Map(clientDto, client);
-            _clientRepository.UpdateClient(client);
+            mapper.Map(clientDto, client);
+            clientRepository.UpdateClient(client);
         }
     }
 
     public void DeleteClient(int id)
     {
-        _clientRepository.DeleteClient(id);
+        clientRepository.DeleteClient(id);
     }
 
     public IEnumerable<string> GetLastSearchParameters(int count)
     {
-        return _clientRepository.GetLastSearchParameters(count);
+        return clientRepository.GetLastSearchParameters(count);
     }
 }
